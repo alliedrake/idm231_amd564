b00642946fbeed2c08bc6c8adc2d968ed39bd928
alliedrake <71614898+alliedrake@users.noreply.github.com>
2021-02-18 19:44:54 -0500

    Added functions for buttons

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ console.log('fade2Content called');
 document.body.classList.remove("blackScreen");
 }
 
+
 function myBirthday() {}
 
-const andyObj = document.getElementById('andy');
-const angelaObj = document.getElementById('angela');
-const dwightObj = document.getElementById('dwight');
-const jimObj = document.getElementById('jim');
-const mikeObj = document.getElementById('mike');
-const oscarObj = document.getElementById('oscar');
-const pamObj = document.getElementById('pam');
-const phyllisObj = document.getElementById('phyllis');
-const ryanObj = document.getElementById('ryan');
-const darrylObj = document.getElementById('darryl');
-const kellyObj = document.getElementById('kelly');
-const stanleyObj = document.getElementById('stanley');
+const andyObj = document.getElementById('ari');
+const darrylObj = document.getElementById('tau');
+const kellyObj = document.getElementById('gem');
+const jimObj = document.getElementById('can');
+const mikeObj = document.getElementById('leo');
+const angelaObj = document.getElementById('vir');
+const phyllisObj = document.getElementById('lib');
+const dwightObj = document.getElementById('sco');
+const ryanObj = document.getElementById('sag');
+const stanleyObj = document.getElementById('cap');
+const oscarObj = document.getElementById('aqu');
+const pamObj = document.getElementById('pis');
 
 
 andyObj.addEventListener('click', function (evt) {
@@ -92,13 +93,64 @@ stanleyObj.addEventListener('click', function (evt) {
 });
 
 
+const userSubmitObj = document.getElementById('submitButton');
 
-/* function userPicked(whichOne) { 
+userSubmitObj.addEventListener('click', function () {
+    console.log('submitButton clicked');
+  
+});
+
+const userFirstName = document.getElementById('fname').value;
+console.log('userFirstName is: ' + userFirstName);
+
+const userBday = document.getElementById('bday').value;
+console.log('userBday is: ' + userBday);
+
+let whichMonth = userBday.getUTCMonth();
+whichMonth++;
+console.log('whichMonth is: ' + whichMonth);
+
+const whichDayOfMonth = userBday.getUTCDate();
+console.log('whichDayOfMonth is: ' + whichDayOfMonth);
+
+let AstroSign = "";
+
+
+if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+    AstroSign = "cap";
+  } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+    AstroSign = "sag";
+  } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+    AstroSign = "sco";
+  } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+    AstroSign = "lib";
+  } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+    AstroSign = "vir";
+  } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+    AstroSign = "leo";
+  } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+    AstroSign = "can";
+  } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+    AstroSign = "gem";
+  } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+    AstroSign = "tau";
+  } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+    AstroSign = "ari";
+  } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+    AstroSign = "pis";
+  } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+    AstroSign = "aqu";
+  }
+
+  console.log('Astro sign is ' + AstroSign);
+
+
+function userPicked(whichOne) { 
     switch (whichOne) {
-        case 'andy':
+        case 'ari':
             default:
     }
 
 }
 
-*/
+
