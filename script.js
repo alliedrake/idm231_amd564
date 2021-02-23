911e5605e7eb66ff5c2e44fc248e1632456753da
alliedrake <71614898+alliedrake@users.noreply.github.com>
2021-02-23 11:48:22 -0500

    Fixed erros in JS and html

--- script.js
+++ script.js
@@ -21,89 +21,88 @@ const pamObj = document.getElementById('pis');
 
 
 andyObj.addEventListener('click', function (evt) {
-    console.log('andy button clicked');  /* + evt.target.id */
+    console.log('andy button clicked' + evt.target.id); 
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 angelaObj.addEventListener('click', function (evt) {
-    console.log('angela button clicked');
+    console.log('angela button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 dwightObj.addEventListener('click', function (evt) {
-    console.log('dwight button clicked');
+    console.log('dwight button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 jimObj.addEventListener('click', function (evt) {
-    console.log('jim button clicked');
+    console.log('jim button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 mikeObj.addEventListener('click', function (evt) {
-    console.log('michael button clicked');
+    console.log('michael button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 oscarObj.addEventListener('click', function (evt) {
-    console.log('oscar button clicked');
+    console.log('oscar button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 pamObj.addEventListener('click', function (evt) {
-    console.log('pam button clicked');
+    console.log('pam button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 phyllisObj.addEventListener('click', function (evt) {
-    console.log('phyllis button clicked');
+    console.log('phyllis button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 ryanObj.addEventListener('click', function (evt) {
-    console.log('ryan button clicked');
+    console.log('ryan button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 darrylObj.addEventListener('click', function (evt) {
-    console.log('darryl button clicked');
+    console.log('darryl button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 kellyObj.addEventListener('click', function (evt) {
-    console.log('kelly button clicked');
+    console.log('kelly button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 stanleyObj.addEventListener('click', function (evt) {
-    console.log('stanley button clicked');
+    console.log('stanley button clicked' + evt.target.id);
     const idName = evt.target.id;
     userPicked(idName);
 });
 
 
-const userSubmitObj = document.getElementById('submitButton');
+
+const userSubmitObj = document.getElementById('userSubmit');
 
 userSubmitObj.addEventListener('click', function () {
     console.log('submitButton clicked');
   
-});
-
 const userFirstName = document.getElementById('fname').value;
 console.log('userFirstName is: ' + userFirstName);
 
-const userBday = document.getElementById('bday').value;
+const userBday = new Date(document.getElementById('bday').value);
 console.log('userBday is: ' + userBday);
 
 let whichMonth = userBday.getUTCMonth();
@@ -112,7 +111,7 @@ console.log('whichMonth is: ' + whichMonth);
 
 const whichDayOfMonth = userBday.getUTCDate();
 console.log('whichDayOfMonth is: ' + whichDayOfMonth);
-
+  
 let AstroSign = "";
 
 
@@ -142,15 +141,18 @@ if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayO
     AstroSign = "aqu";
   }
 
-  console.log('Astro sign is ' + AstroSign);
+  console.log('AstroSign is ' + AstroSign);
+
+});
 
 
-function userPicked(whichOne) { 
+
+/* function userPicked(whichOne) { 
     switch (whichOne) {
         case 'ari':
-            default:
+          display
+    
     }
 
 }
-
-
+*/
