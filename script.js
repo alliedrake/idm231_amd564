bfab2eb8a52163832ba71cf550f85b3f88420f23
alliedrake <71614898+alliedrake@users.noreply.github.com>
2021-03-06 19:44:26 -0500

    Update script.js
    Added sounds and redid modals

--- script.js
+++ script.js
@@ -94,7 +94,10 @@ stanleyObj.addEventListener('click', function (evt) {
     userPicked(idName);
 });
 
+const displayObj = document.getElementById('display');
 
+
+// Calendar Input //
 
 const userSubmitObj = document.getElementById('userSubmit');
 
@@ -145,204 +148,253 @@ if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayO
 
   console.log('AstroSign is ' + AstroSign);
 
+  userPicked(AstroSign);
+
 });
+
+// Modal Control
 
 
 const modal = document.getElementById('modal');
+const closeModalObj = document.getElementById('btn-modal-close');
+closeModalObj.addEventListener('click', function() {
+  console.log('Close this modal!');
+  stopAllAudio();
+  modal.hidden = !modal.hidden;
+});
 
-const btnModal = document.querySelectorAll('.zButton');
-btnModal.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
+const zDisplayImgObj = document.getElementById('zDisplayImg');
+const zTitleObj = document.getElementById('zTitle');
+const zDateRangeObj = document.getElementById('zDateRange');
+const zMoreInfoObj = document.getElementById('zMoreInfo');
+
+
+
+function userPicked(whichOne) {
+  console.log('userPicked called: ' + whichOne);
+
+  switch (whichOne) {
+    case 'ari':
+      zDisplayImgObj.src = "images/andybernardcircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Andy Bernard</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>March 20th - April 19th</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>Just like Andy, you are ready to step in as a leader, aren't afraid to be bold, and have a childlike personality. While you're sometimes naive and quick-tempered, your optimistic and enthusiatic nature draw those around you in.</p>";
+      // play sound
+      stopAllAudio();
+      andObj.play();
       modal.hidden = !modal.hidden;
-    },
-    false
-  );
-});
+      break;
 
-const modal2 = document.getElementById('modal2');
+      case 'tau':
+      zDisplayImgObj.src = "images/darrylphilbincircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Darryl Philbin</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>April 20th - May 20th</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>You're the person that holds everything down, just as Daryll does whether it's in the warehouse or in the office. You're patient when it comes to dealing with the people around you, and although sometimes you find yourself being stubborn, you stay persistent and act as a friend that people can rely on.</p>";
+      // play sound
+      stopAllAudio();
+      darObj.play();
+    modal.hidden = !modal.hidden;
+    break;
 
-const btnModal_2 = document.querySelectorAll('.bButton');
+     case 'gem':
+      zDisplayImgObj.src = "images/kellykapoorcircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Kelly Kapoor</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>May 21st - June 20th</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>Your talkatative and curious nature sparks new connections, and just like Kelly you are all about socializing within your environment. While people mistake you for being two-faced, your personality is able to adapt and it's easy to express it.</p>";
+      // play sound
+      stopAllAudio();
+      kelObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
+      case 'can':
+      zDisplayImgObj.src = "images/jimcircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Jim Halpert</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>June 21st - July 22nd</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>Just like Jim, you have it all. While you're sarcastic and witty on the outside, your're passionate and sentimental to the core. Someone like you can either be a best friend or worst enemy - according to Dwight. You will do whatever you can to make other's be the best they can be, even if it's unrealistic.</p>";
+      // play sound
+      stopAllAudio();
+      jiObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
-btnModal_2.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal2.hidden = !modal2.hidden;
-    },
-    false
-  );
-});
+      case 'leo':
+      zDisplayImgObj.src = "images/michaelscottcircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Michael Scott</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>July 23rd - August 22nd</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>It's Michael Scott's world, and everyone else is just living in it. You require a lot of attention and while you can be seen as controlling or dramatic, you are fearless and use your confidence and humor to light up those around you. You were born to rule through your ambitious nature.</p>";
+      // play sound
+      stopAllAudio();
+      micObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
-const modal3 = document.getElementById('modal3');
+      case 'vir':
+      zDisplayImgObj.src = "images/angelamartincircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1Angela Martin</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>August 23rd - September 22nd</h2>";
+      // ddescription
+      zMoreInfoObj.innerHTML = "<p>While Angela can be cold and over-critical, there is a different side to you that says otherwise. You are practical, diligent, sensible, and great helpers to those in need. While you're subtle in your actions, you notice and remember everything which gives you an advantage.</p>";
+      // play sound
+      stopAllAudio();
+      angObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
-const btnModal_3 = document.querySelectorAll('.cButton');
+      case 'lib':
+      zDisplayImgObj.src = "images/phyllisvancecircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Phyllis Vance</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>September 23rd - October 22nd</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>Phyllis and you are the most balanced zodiac sign in the office. Sociable, easy-going, polite are just a few that words make up for who you are. Indecisiveness and gullibility can get in your way at times but you're laid-back nature draws people in which contributes to your likeableness.</p>";
+      // play sound
+      stopAllAudio();
+      phyObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
+      case 'sco':
+      zDisplayImgObj.src = "images/dwightschrutecircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Dwight Schrute</h1>";
+      // display date range
+      zDateRangeObj.innerHTML = "<h2>October 23rd - November 21st</h2>";
+      // display description
+      zMoreInfoObj.innerHTML = "<p>You are highly intelligent and have a very strong understanding of your inner self. Dwight is highly competitive and intense, and someone like you doesn't fall short from that. While your domineering nature may come off as aggressive, you're passionate about the things you care about - who could blame you!</p>";
+      // play sound
+      stopAllAudio();
+      dwiObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
-btnModal_3.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal3.hidden = !modal3.hidden;
-    },
-    false
-  );
-});
+      case 'sag':
+      zDisplayImgObj.src = "images/ryanhowardcircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Ryan Howard</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>November 22nd - December 21st</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>Who once first started as a temp, Ryan, like yourself, breaks barriers. You are fiery and charming but on your bad days you tend to show careless, hot-headed, and pushy tendencies. Otherwise, you draw people in with your extraverted and confident personality.</p>";
+      // play sound
+      stopAllAudio();
+      ryaObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
-const modal4 = document.getElementById('modal4');
+      case 'cap':
+      zDisplayImgObj.src = "images/stanleyhudsoncircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Stanley Hudson</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>December 22nd - January 19th</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>Stanley is a tough man to please, and like most of you, you're sometimes pessimistic and stubborn in situations. Make no mistake, you're practical and wise which makes others look up to you. Like Stanely, you're very passionate about your interests and are an independent individual.</p>";
+      // play sound
+      stopAllAudio();
+      staObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
-const btnModal_4 = document.querySelectorAll('.dButton');
+      case 'aqu':
+      zDisplayImgObj.src = "images/oscarmartinezcircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Oscar Martinez</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>January 20th - February 18th</h2>";
+      // description
+      zMoreInfoObj.innerHTML = "<p>Oscar is on top of things - and just like him, you know your stuff. You have high standards - you're intellectual and independent nature makes you likeable and a leader. Sometimes people see you as contradictory and stubborn, but you're better at seeing through people through your observant ways.</p>";
+      // play sound
+      stopAllAudio();
+      oscObj.play();
+      modal.hidden = !modal.hidden;
+      break;
 
-
-btnModal_4.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal4.hidden = !modal4.hidden;
-    },
-    false
-  );
-});
-
-const modal5 = document.getElementById('modal5');
-
-const btnModal_5 = document.querySelectorAll('.eButton');
-
-
-btnModal_5.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal5.hidden = !modal5.hidden;
-    },
-    false
-  );
-});
-
-const modal6 = document.getElementById('modal6');
-
-const btnModal_6 = document.querySelectorAll('.fButton');
-
-
-btnModal_6.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal6.hidden = !modal6.hidden;
-    },
-    false
-  );
-});
-
-const modal7 = document.getElementById('modal7');
-
-const btnModal_7 = document.querySelectorAll('.gButton');
-
-
-btnModal_7.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal7.hidden = !modal7.hidden;
-    },
-    false
-  );
-});
-
-const modal8 = document.getElementById('modal8');
-
-const btnModal_8 = document.querySelectorAll('.hButton');
-
-
-btnModal_8.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal8.hidden = !modal8.hidden;
-    },
-    false
-  );
-});
-
-const modal9 = document.getElementById('modal9');
-
-const btnModal_9 = document.querySelectorAll('.jButton');
-
-
-btnModal_9.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal9.hidden = !modal9.hidden;
-    },
-    false
-  );
-});
-
-const modal10 = document.getElementById('modal10');
-
-const btnModal_10 = document.querySelectorAll('.kButton');
-
-
-btnModal_10.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal10.hidden = !modal10.hidden;
-    },
-    false
-  );
-});
-
-const modal11 = document.getElementById('modal11');
-
-const btnModal_11 = document.querySelectorAll('.lButton');
-
-
-btnModal_11.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal11.hidden = !modal11.hidden;
-    },
-    false
-  );
-});
-
-const modal12 = document.getElementById('modal12');
-
-const btnModal_12 = document.querySelectorAll('.mButton');
-
-
-btnModal_12.forEach(button => {
-  button.addEventListener(
-    'click',
-    () => {
-      modal12.hidden = !modal12.hidden;
-    },
-    false
-  );
-});
+      case 'pis':
+      zDisplayImgObj.src = "images/pambeeslycircle.png";
+      // title
+      zTitleObj.innerHTML = "<h1>Pam Beesly</h1>";
+      // date range
+      zDateRangeObj.innerHTML = "<h2>February 19th - March 20th</h2>";
+      // description
+      zMoreInfoObj.innerHTML = " <p>You have a comforting presence to you and are considered to be a very creative person - just like Pam. You draw people in due to your compassionate, trusting, and generous nature. Everyone can count on you, and you know how to protect your vulnerability through humour and a cool exterior.</p>";
+      // play sound
+      stopAllAudio();
+      paObj.play();
+      modal.hidden = !modal.hidden;
+      break;
+  }
+}
 
 
+// Sounds 
 
+const andObj = document.getElementById('sndAndy');
+const darObj = document.getElementById('sndDarryl');
+const kelObj = document.getElementById('sndKelly');
+const jiObj = document.getElementById('sndJim');
+const micObj = document.getElementById('sndMike');
+const angObj = document.getElementById('sndAngela');
+const phyObj = document.getElementById('sndPhyllis');
+const dwiObj = document.getElementById('sndDwight');
+const ryaObj = document.getElementById('sndRyan');
+const staObj = document.getElementById('sndStanley');
+const oscObj = document.getElementById('sndOscar');
+const paObj = document.getElementById('sndPam');
 
+function stopAllAudio() {
+  // Stop ALL sounds for playing
+  andObj.pause();
+  andObj.currentTime = 0;
+  //
+  darObj.pause();
+  darObj.currentTime = 0;
+  //
+  kelObj.pause();
+  kelObj.currentTime = 0;
 
+  jiObj.pause();
+  jiObj.currentTime = 0;
 
+  micObj.pause();
+  micObj.currentTime = 0;
 
+  angObj.pause();
+  angObj.currentTime = 0;
 
+  phyObj.pause();
+  phyObj.currentTime = 0;
 
+  dwiObj.pause();
+  dwiObj.currentTime = 0;
 
-/*
- function userPicked(whichOne) { 
-    switch (whichOne) {
-        case 'ari':
-          displayObj.src = 'modal';
-          default: 
-    
-    };
+  ryaObj.pause();
+  ryaObj.currentTime = 0;
 
-};
-*/
+  staObj.pause();
+  staObj.currentTime = 0;
+
+  oscObj.pause();
+  oscObj.currentTime = 0;
+
+  paObj.pause();
+  paObj.currentTime = 0;
+}
